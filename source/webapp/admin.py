eedbb1d3c923bf50d15529dd4f0d60b8d41d2a12
adi.kulubekov <adi.kulubekov@gmail.com>
2022-11-04 00:19:58 +0600

    craet model

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from webapp.models import Article
 
 # Register your models here.
+admin.site.register(Article)
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['describe', 'status', 'schedule_at']
+    list_filter = ['status']
+    search_fields = ['describe']
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+status_choices = [('new', 'Новая'), ('in_progress', 'В процессе'), ('done', 'Сделано')]
+
+
 # Create your models here.
+class Article(models.Model):
+    describe = models.CharField(max_length=50, null=False, blank=False, verbose_name='describe')
+    status = models.CharField(max_length=3000, blank=False, verbose_name='status', choices=status_choices,
+                              default=status_choices[0][0])
+    schedule_at = models.DateTimeField(verbose_name='schedule_at', auto_now_add=True)
+
+    def __str__(self):
+        return self.describe + ' ' + self.status + ' ' + self.schedule_at
